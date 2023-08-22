4fc40289acf5abdfee8256f466f00ec12bec8725
Hazbunallahi <Harzbun@gmail.com>
2023-08-22 19:05:27 +0100

    lastess

--- Home/settings.py
+++ Home/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tzd)7ntb3^!6%0sg#4zd3fe-f_i!6bhav2lzrv=)+k!v1z(%g@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
